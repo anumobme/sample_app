26b96ffdaebb6185d21bf367c458dea1ddb0ec59
Anu <anu.raveendran@mobme.in>
2013-10-09 10:30:23 +0530

    Finish user edit, update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130930093915) do
+ActiveRecord::Schema.define(version: 20131008104145) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20130930093915) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,8 +15,10 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
 
   it { should be_valid }
+  it { should_not be_admin }
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -90,6 +92,15 @@ describe "with a password that's too short" do
       @user.save
       expect(@user.reload.email).to eq mixed_case_email.downcase
     end
+  end
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
   end
 
 
